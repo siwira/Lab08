9c8fe3f31f787623b939ed36d1fd4af7e8f5751d
siwira <siwira@ualberta.ca>
2026-02-21 19:44:24 -0700

    Commit Failed Test (deleteCity)

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -10,4 +10,13 @@ public class CustomListTest {
         list.addCity(calgary);// This will fail initially because hasCity() doesn't exist
         assertTrue(list.hasCity(calgary));
     }
+    @Test
+    public void testDeleteCity(){
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertTrue(list.hasCity(calgary));
+        list.deleteCity(calgary); // this will fail
+        assertFalse(list.hasCity(calgary));
+    }
 }
